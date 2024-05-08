191d7b562913d28ab0fd88352fd5915b65040bc4
Max Hernandez <maxhernandezcastillo@gmail.com>
2024-05-07 23:30:42 -0600

    added git action to run tests on commit

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,20 @@
-global.__BINARY_PATH__ = 
-  '/Users/maxkalavera/workplace/mindbuffer/out/MindBuffer-darwin-arm64/MindBuffer.app/Contents/MacOS/MindBuffer'
+import path from 'path';
+import { platform, arch } from 'os';
+
+import JSONPackage from '../../package.json'
+
+const BASE_BINARY_PATH = path.resolve(__dirname, '../../out', `${JSONPackage.productName}-${platform()}-${arch()}`)
+
+switch(platform()) {
+  case 'linux':
+    global.__BINARY_PATH__ = path.resolve(BASE_BINARY_PATH, `${JSONPackage.productName}`)
+    break;
+  case 'darwin':
+    global.__BINARY_PATH__ = path.resolve(BASE_BINARY_PATH, 'MindBuffer.app/Contents/MacOS/MindBuffer')
+    break;
+  case 'win32':
+    global.__BINARY_PATH__ = path.resolve(BASE_BINARY_PATH, `${JSONPackage.productName}.exe`)
+    break;
+}
 
 jest.retryTimes(3)
